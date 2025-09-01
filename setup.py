594ead817c845876c8e394d7477126482e7dd6ab
Kaydance Turner <kaydance.turner@launchfirestorm.com>
2025-09-01 18:23:18 -0400

    New Release v1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='pypaperretriever',
-    version='0.1.0',
+    version='1.0.0',
     author='Joseph Turner',
     author_email='josephisaacturner@gmail.com',
     packages=find_packages(),
